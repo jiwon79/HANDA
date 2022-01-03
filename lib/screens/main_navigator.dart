cb103e70380ca68952985ea45336e7236f605368
jiwon79 <19-079@ksa.hs.kr>
2022-01-04 02:15:05 +0900

    feat: Add bottom navigation function

--- lib/screens/main_navigator.dart
+++ lib/screens/main_navigator.dart
@@ -10,6 +10,13 @@ class MainNavigator extends StatefulWidget {
 class _MainNavigatorState extends State<MainNavigator> {
   int _selectedIndex = 0;
 
+  final tabs = [
+    Center(child: Text('aaa')),
+    Center(child: Text('bbb')),
+    Center(child: Text('ccc')),
+    Center(child: Text('ddd')),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -22,28 +29,37 @@ class _MainNavigatorState extends State<MainNavigator> {
       appBar: AppBar(
         title: Text('Main Page'),
       ),
-      body: Container(),
+      body: tabs[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home, color: Colors.black),
             label: 'home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today, color: Colors.black,),
+            icon: Icon(
+              Icons.calendar_today,
+              color: Colors.black,
+            ),
             label: 'calendar',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline_rounded, color: Colors.black,),
+            icon: Icon(
+              Icons.person_outline_rounded,
+              color: Colors.black,
+            ),
             label: 'ccc',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_search, color: Colors.black,),
+            icon: Icon(
+              Icons.person_search,
+              color: Colors.black,
+            ),
             label: 'ddd',
           ),
         ],
