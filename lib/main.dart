cb2666dec0cb7347d33d5987afb76458a72ef432
jiwon79 <19-079@ksa.hs.kr>
2022-01-04 23:33:00 +0900

    feat: Delete debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/landing',
       routes: {
         '/landing': (context) => LandingPage(),
