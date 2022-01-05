8ed29be2ec2d908ef9795d4671d8d3e687372ff4
jiwon79 <19-079@ksa.hs.kr>
2022-01-06 03:22:49 +0900

    feat: Add register page content

--- lib/screens/landing/register.dart
+++ lib/screens/landing/register.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _checkPwController = TextEditingController();
+
+  bool _isCheckedPw = false;
+  bool _isOverlapId = true;
+  List<String> pwDialog = ['비밀번호가 일치하지 않습니다.', '비빌번호가 일치합니다.'];
+
+  void checkIsSamePw (String text) {
+    setState(() {
+      if (_pwController.text == _checkPwController.text) {
+        _isCheckedPw = true;
+      } else {
+        _isCheckedPw = false;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Register page'),
-      ),
-      body: Container()
+        appBar: AppBar(
+          title: Text('회원가입'),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  labelText: '아이디'
+              ),
+              controller: _idController,
+            ),
+            Text(_isOverlapId ? '아이디가 중복' : '아이디 중복 아님'),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: '이메일'
+              ),
+              controller: _mailController,
+              onChanged: checkIsSamePw,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: '비밀번호'
+              ),
+              controller: _pwController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: '비밀번호 확인'
+              ),
+              controller: _checkPwController,
+              onChanged: checkIsSamePw,
+            ),
+            Text(_isCheckedPw ? '비밀번호가 일치' : '비밀번호가 불일치'),
+            OutlinedButton(
+              child: Text('로그인'),
+              onPressed: () {
+                print(_idController.text);
+              },
+            )
+          ],
+        )
     );
   }
 }
