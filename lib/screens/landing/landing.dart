5e202eef7aebb3286644d4b6d158c363bae93018
jiwon79 <19-079@ksa.hs.kr>
2022-01-12 01:14:06 +0900

    feat: Add landing page position style

--- lib/screens/landing/landing.dart
+++ lib/screens/landing/landing.dart
@@ -8,39 +8,89 @@ class LandingPage extends StatelessWidget {
 
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
         decoration: BoxDecoration(
         image: DecorationImage(
             image: AssetImage('assets/images/landing_background.png'),
             fit: BoxFit.cover),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Center(
               child: Text('handa'),
             ),
-            Text(
-              '완벽한 실천을 위한 \n완벽한 플래너',
-              style: TextStyle(
-                fontSize: 30,
-                // fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontFamily: 'noto',
-                fontWeight: FontWeight.w700,
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
+              child: Text(
+                '완벽한 실천을 위한 \n완벽한 플래너',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontFamily: 'noto',
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-            TextButton(
-              child: Text('로그인'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/landing/login');
-              },
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 40,
+                margin: EdgeInsets.fromLTRB(0, 94, 0, 9),
+                child: ElevatedButton(
+                  child: Text(
+                      '로그인',
+                    style: TextStyle(
+                      fontFamily: 'noto',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/landing/login');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.disabled)) {
+                        return Colors.grey;
+                      } else {
+                        return Color(0xffE4581B);
+                      }
+                    })
+                  )
+                ),
+              ),
             ),
-            TextButton(
-              child: Text('회원가입'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/landing/register');
-              },
+            Center(
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.fromLTRB(0, 9, 0, 9),
+                child: ElevatedButton(
+                  child: Text(
+                    '회원가입',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'noto',
+                      fontWeight: FontWeight.w700
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/landing/register');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.disabled)) {
+                        return Colors.grey;
+                      } else {
+                        return Color(0xffF9F9F9);
+                      }
+                    }),
+                  )
+                ),
+              ),
             )
           ],
         ),
