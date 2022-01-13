64a638bb63c88f337db125d7ecc244e3acdb6305
jiwon79 <19-079@ksa.hs.kr>
2022-01-13 23:02:02 +0900

    feat: Add todo model

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -2,12 +2,22 @@ class Todo {
   Todo({
     required this.name,
     required this.isDone,
+    required this.isEditing,
   });
 
   String name;
   bool isDone = false;
+  bool isEditing;
 
   void toggleDone() {
     isDone = !isDone;
+  }
+
+  void toggleEditing() {
+    isEditing = !isEditing;
+  }
+
+  void updateName(String updateName) {
+    name = updateName;
   }
 }
--- lib/provider/todo.dart
+++ lib/provider/todo.dart
@@ -3,11 +3,11 @@ import 'package:flutter_todo/models/todo_model.dart';
 
 class TodoData with ChangeNotifier {
   List<Todo> _todos = [
-    Todo(name: '자기', isDone: false),
-    Todo(name: '잠자기', isDone: false),
-    Todo(name: '낮잠자기', isDone: false),
-    Todo(name: '저녘에 잠자리에 들기', isDone: false),
-    Todo(name: '잠 푹 자기', isDone: false),
+    Todo(name: '자기', isDone: false, isEditing: true),
+    Todo(name: '잠자기', isDone: false, isEditing: false),
+    Todo(name: '낮잠자기', isDone: false, isEditing: false),
+    Todo(name: '저녘에 잠자리에 들기', isDone: false, isEditing: false),
+    Todo(name: '잠 푹 자기', isDone: false, isEditing: false),
   ];
 
   List<Todo> get todos => _todos;
@@ -16,8 +16,8 @@ class TodoData with ChangeNotifier {
     return _todos.length;
   }
 
-  void addTodo(String newTodoName) {
-    _todos.add(Todo(name: newTodoName, isDone: false));
+  void addTodo() {
+    _todos.add(Todo(name: 'new', isDone: false, isEditing: true));
     notifyListeners();
   }
 
@@ -26,8 +26,15 @@ class TodoData with ChangeNotifier {
     notifyListeners();
   }
 
+  void updateTodoName(Todo todo, String updateName) {
+    todo.updateName(updateName);
+    todo.toggleEditing();
+    notifyListeners();
+  }
+
   void deleteTodo(Todo todo) {
     _todos.remove(todo);
     notifyListeners();
   }
+
 }
