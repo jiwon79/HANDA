6e779c28840c4877848abafce0634570e6ebd03f
jiwon79 <19-079@ksa.hs.kr>
2022-01-30 01:18:10 +0900

    feat: Add current user and user data model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -21,3 +21,24 @@ class MyDataResponse {
     registered_at: json['registered_at'],
   );
 }
+
+class UserDataResponse {
+  UserDataResponse({
+    required this.username,
+    required this.nickname,
+    required this.is_verified,
+    required this.registered_at,
+  });
+
+  String username;
+  String nickname;
+  bool is_verified;
+  String registered_at;
+
+  factory UserDataResponse.fromJson(Map<String, dynamic> json) => UserDataResponse(
+    username: json['username'],
+    nickname: json['nickname'],
+    is_verified: json['is_verified'],
+    registered_at: json['registered_at'],
+  );
+}
