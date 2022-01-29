47c23f714e106bcba938d172e46003f8d9138d8b
jiwon79 <19-079@ksa.hs.kr>
2022-01-30 02:27:38 +0900

    feat: Add followings and followers api endpoint method

--- lib/services/user_api.dart
+++ lib/services/user_api.dart
@@ -25,4 +25,38 @@ class UserApi {
       throw(Exception('UnExpected error!!!'));
     }
   }
+
+  Future getMyFollowingList() async {
+    late Response response;
+    late UserList followingListResponse;
+
+    try {
+      response = await http.getRequest('/users/me/followings');
+      if (response.statusCode == 200) {
+        followingListResponse = UserList.fromJson(response.data);
+        response.data = followingListResponse;
+      }
+      return response;
+    } on DioError catch(e) {
+      print(e.response);
+      throw(Exception('UnExpected error!!!'));
+    }
+  }
+
+  Future getMyFollowerList() async {
+    late Response response;
+    late UserList followerListResponse;
+
+    try {
+      response = await http.getRequest('/users/me/followers');
+      if (response.statusCode == 200) {
+        followerListResponse = UserList.fromJson(response.data);
+        response.data = followerListResponse;
+      }
+      return response;
+    } on DioError catch(e) {
+      print(e.response);
+      throw(Exception('UnExpected error!!!'));
+    }
+  }
 }
