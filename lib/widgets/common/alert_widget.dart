d65e0aeac6d400471d5ae5831eb2bd36b21cdfc2
jiwon79 <19-079@ksa.hs.kr>
2022-02-03 18:45:03 +0900

    feat: add show dialog common function

---
+++ lib/widgets/common/alert_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void defaultOnPress(context) {
+  Navigator.pop(context);
+}
+
+void alertWidget({
+  required context,
+  required title,
+  content,
+  onPress = defaultOnPress,
+}) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: content == null ? null : Text(content),
+        actions: [
+          TextButton(
+            child: Text("확인"),
+            onPressed: () {
+              defaultOnPress(context);
+            },
+          )
+        ],
+      );
+    }
+  );
+}
