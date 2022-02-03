4a648cd8a34b15aaeb731c0cc7c5d1f6c873289d
jiwon79 <19-079@ksa.hs.kr>
2022-02-03 22:50:01 +0900

    feat: add goto login button in register page

--- lib/screens/landing/register.dart
+++ lib/screens/landing/register.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/models/auth_model.dart';
 import 'package:flutter_todo/services/auth_api.dart';
+import 'package:flutter_todo/widgets/common/alert_widget.dart';
+import 'package:flutter_todo/widgets/common/suffix_button.dart';
 import 'package:flutter_todo/widgets/common/textField_widget.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -18,12 +20,12 @@ class _RegisterPageState extends State<RegisterPage> {
     'password': '',
     're_password': '',
   };
+  bool _isOverlapId = true;
 
   Function handleState(state) {
     void handleStateFunction(text) {
       setState(() {
         _userData[state] = text;
-        print(_userData);
       });
     }
     return handleStateFunction;
@@ -41,44 +43,33 @@ class _RegisterPageState extends State<RegisterPage> {
     };
     var response = await AuthApi().registerRequest(data);
 
-    if (response == 400) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('회원가입 오류'),
-            content: Text('아이디 혹은 이메일이 이미 존재합니다.'),
-            actions: [
-              TextButton(
-                child: Text("확인"),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          );
-        }
-      );
-    } else {
-      showDialog(
+    if (response == 201) {
+      alertWidget(
           context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              content: Text('회원가입이 되었습니다.'),
-              actions: [
-                TextButton(
-                  child: Text("확인"),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                    Navigator.pushNamed(context, '/landing/login');
-                  },
-                )
-              ],
-            );
+          title: '회원가입이 되었습니다.',
+          onPress: () {
+            Navigator.pop(context);
+            Navigator.pop(context);
+            Navigator.pushNamed(context, '/landing/login');
           }
       );
+    } else if (response == 400) {
+      alertWidget(
+          context: context,
+          title: '회원가입 오류',
+          content: '아이디 혹은 이메일이 이미 존재합니다.'
+      );
+    } else {
+      alertWidget(
+          context: context,
+          title: '알 수 없는 오류',
+          content: '회원가입을 한번 더 부탁드립니다.'
+      );
     }
+  }
+
+  void checkNicknameIsOverlap() {
+    print(1);
   }
 
 
@@ -97,21 +88,33 @@ class _RegisterPageState extends State<RegisterPage> {
             TextFieldWidget(label: '이름', onChanged: handleState('username')),
             TextFieldWidget(
               label: '아이디',
-              onChanged: handleState('nickname')
+              onChanged: handleState('nickname'),
+              suffix: SuffixButton(
+                content: '아이디 중복 확인',
+                onPress: checkNicknameIsOverlap,
+              ),
             ),
             Text('아이디 중복 확인이 되지 않았습니다.'),
             TextFieldWidget(label: '이메일 입력', onChanged: handleState('email')),
             TextFieldWidget(label: '비밀번호 입력', onChanged: handleState('password')),
             TextFieldWidget(label: '비밀번호 확인', onChanged: handleState('re_password')),
             Text(_userData['password'] == _userData['re_password']
-              ? '비밀번호가 일치'
-              : '비밀번호가 불일치'
+              ? '비밀번호가 일치합니다.'
+              : '비밀번호가 일치하지 않습니다.'
             ),
             TextButton(
               child: Text('회원가입'),
               onPressed: () {
                 register();
               },
+            ),
+            Text('이미 게정을 가지고 계신가요?'),
+            TextButton(
+              onPressed: () {
+                Navigator.popUntil(context, (route) => route.isFirst);
+                Navigator.pushNamed(context, '/landing/login');
+              },
+              child: Text('로그인하러 가기')
             )
           ],
         ),
