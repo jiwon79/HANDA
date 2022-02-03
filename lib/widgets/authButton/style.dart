04ebb43c588340dcc4afdabdf6a87259ff2e48ec
jiwon79 <19-079@ksa.hs.kr>
2022-02-03 22:57:08 +0900

    feat : change style var to function have isRounded input variable

--- lib/widgets/authButton/style.dart
+++ lib/widgets/authButton/style.dart
@@ -2,43 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo/utils/enums.dart';
 
 ButtonStyle authButtonStyle(AuthAction action) {
-  ButtonStyle orangeButtonStyle = ButtonStyle(
-    backgroundColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.disabled)) {
-        return Colors.grey;
-      } else {
-        return Color(0xffE4581B);
-      }
-    }),
-    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-      RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(0),
-      )
-    )
-  );
+  ButtonStyle orangeButtonStyle(bool isRounded) {
+    return ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.disabled)) {
+            return Colors.grey;
+          } else {
+            return Color(0xffE4581B);
+          }
+        }),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(isRounded ? 20 : 0),
+            )
+        )
+    );
+  }
 
-  ButtonStyle whiteButtonStyle = ButtonStyle(
-    backgroundColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.disabled)) {
-        return Colors.grey;
-      } else {
-        return Color(0xffF9F9F9);
-      }
-    }),
-    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-      RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(0),
-      )
-    )
-  );
+  ButtonStyle whiteButtonStyle(bool isRounded) {
+    return ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.disabled)) {
+            return Colors.grey;
+          } else {
+            return Color(0xffF9F9F9);
+          }
+        }),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(isRounded ? 20 : 0),
+            )
+        )
+    );
+  }
 
   switch (action) {
     case AuthAction.landingLogin:
-      return orangeButtonStyle;
+      return orangeButtonStyle(false);
     case AuthAction.landingRegister:
-      return whiteButtonStyle;
+      return whiteButtonStyle(false);
     case AuthAction.login:
-      return orangeButtonStyle;
+      return orangeButtonStyle(false);
+    case AuthAction.register:
+      return orangeButtonStyle(true);
     default:
       return ButtonStyle();
   }
