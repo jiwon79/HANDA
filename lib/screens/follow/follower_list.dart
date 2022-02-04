1d618d13bff015b0efc50a9ec829b8c22a99bb33
jiwon79 <19-079@ksa.hs.kr>
2022-02-05 03:12:22 +0900

    feat: add loading anim when open follower page

--- lib/screens/follow/follower_list.dart
+++ lib/screens/follow/follower_list.dart
@@ -3,21 +3,35 @@ import 'package:flutter_todo/provider/user.dart';
 import 'package:flutter_todo/widgets/my_page/follow_item.dart';
 import 'package:provider/provider.dart';
 
-class FollowerList extends StatelessWidget {
+class FollowerList extends StatefulWidget {
   const FollowerList({Key? key}) : super(key: key);
 
   @override
+  _FollowerListState createState() => _FollowerListState();
+}
+
+class _FollowerListState extends State<FollowerList> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: Provider.of<UserData>(context).user.detail.num_followers,
-        itemBuilder: (BuildContext context, int index) {
-          return FollowItem(
-            user: Provider.of<UserData>(context).followerList[index],
-            option: FollowOptions.follower,
+    return FutureBuilder(
+      future: Provider.of<UserData>(context, listen: false).getFollowers(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Network Error');
+        } else {
+          return ListView.builder(
+            itemCount: Provider.of<UserData>(context).user.detail.num_followers,
+            itemBuilder: (BuildContext context, int index) {
+              return FollowItem(
+                user: Provider.of<UserData>(context).followerList[index],
+                option: FollowOptions.follower,
+              );
+            }
           );
         }
-      )
+      },
     );
   }
 }
