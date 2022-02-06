e0db19d8dcb5515e3d2dee98a883ef34ab5d738a
jiwon79 <19-079@ksa.hs.kr>
2022-02-07 02:15:54 +0900

    refactor: move calendar folder from todo folder

---
+++ lib/widgets/calendar/calendar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_todo/provider/calendar.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      selectedDayPredicate: (day) {
+        return isSameDay(context.watch<CalendarData>().selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        context.read<CalendarData>().changeSelectedDay(selectedDay);
+        context.read<CalendarData>().changeFocusedDay(focusedDay);
+      },
+      onPageChanged: (focusedDay) {
+        context.read<CalendarData>().changeFocusedDay(focusedDay);
+      },
+      onFormatChanged: (format) {
+        if (context.watch<CalendarData>().calendarFormat != format) {
+          context.read<CalendarData>().changeCalendarFormat(format);
+        }
+      },
+      firstDay: DateTime.utc(2022, 1, 1),
+      lastDay: DateTime.utc(2022, 4, 1),
+      focusedDay: context.watch<CalendarData>().focusedDay,
+      calendarFormat: context.watch<CalendarData>().calendarFormat,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      currentDay: DateTime.now().add(Duration(hours: 9)),
+      headerVisible: false,
+      calendarBuilders: CalendarBuilders(
+        dowBuilder: (context, day) {
+          if (day.weekday == DateTime.sunday) {
+            final text = DateFormat.E().format(day);
+            return Center(
+              child: Text(
+                text,
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+          }
+        },
+        defaultBuilder: (context, day, focusedDay) {
+          return Container(
+            color: Colors.blue,
+            margin: EdgeInsets.all(3),
+            child: Center(
+              child: Text(day.day.toString()),
+            ),
+          );
+        },
+        // todayBuilder: (context, day, focusedDay) {
+        //   return Center(
+        //     child: Text(day.day.toString()),
+        //   );
+        // },
+        // selectedBuilder: (context, day, focusedDay) {
+        //   return Center(
+        //     child: Text(day.day.toString()),
+        //   );
+        // },
+      ),
+    );
+  }
+}
