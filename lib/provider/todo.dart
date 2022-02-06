f6cb97bb459481f4300492b76363d0d4443e178c
jiwon79 <19-079@ksa.hs.kr>
2022-02-07 04:45:00 +0900

    refactor: replace _todos to _todoMap in todo provider

--- lib/provider/todo.dart
+++ lib/provider/todo.dart
@@ -6,13 +6,13 @@ import 'package:intl/intl.dart';
 
 class TodoData with ChangeNotifier {
   Map<String, List<Todo>> _todoMap= {};
-  List<Todo> _todos = [];
-
   Map<String, List<Todo>> get todoMap => _todoMap;
-  List<Todo> get todos => _todos;
 
   Future getTodoList() async {
     Response<dynamic> response =  await TodoApi().getTodoData();
+    for (String date in _todoMap.keys) {
+      _todoMap[date] = [];
+    }
     for (Todo todo in response.data.todoList) {
       if (_todoMap[todo.dueDate] == null) {
         _todoMap[todo.dueDate] = [todo];
@@ -20,16 +20,16 @@ class TodoData with ChangeNotifier {
         _todoMap[todo.dueDate]!.add(todo);
       }
     }
-    _todos = response.data.todoList;
   }
 
   int get todoCount {
-    return _todos.length;
+    int count = 0;
+    for (List<Todo> element in _todoMap.values) {
+      count += element.length;
+    }
+    return count;
   }
 
-  int get todoDoneCount {
-    return _todos.where((Todo element) => element.isDone).length;
-  }
 
   List<Todo> getTodosSelectedDay(DateTime date) {
     String dateFormat = DateFormat('yyyy-MM-dd').format(date);
@@ -41,7 +41,6 @@ class TodoData with ChangeNotifier {
   }
 
 
-
   void addTodo(DateTime date) async {
     var requestData = {
       'title': 'new',
@@ -50,15 +49,22 @@ class TodoData with ChangeNotifier {
     var todoResponse = await TodoApi().addTodoRequest(requestData);
     print(todoResponse);
 
-    _todos.add(Todo(
-        name: todoResponse.data.name,
-        isDone: todoResponse.data.isDone,
-        isEditing: true,
-        id: todoResponse.data.id,
-        dueDate: todoResponse.data.dueDate,
-    ));
+    if (_todoMap[todoResponse.data.dueDate] == null) {
+      _todoMap[todoResponse.data.dueDate] = [];
+    }
+
+    _todoMap[todoResponse.data.dueDate]!.add(
+        Todo(
+          name: todoResponse.data.name,
+          isDone: todoResponse.data.isDone,
+          isEditing: true,
+          id: todoResponse.data.id,
+          dueDate: todoResponse.data.dueDate,
+        )
+    );
     notifyListeners();
   }
+
 
   void updateTodo(Todo todo) async {
     Map<String, dynamic> requestData = {
@@ -70,6 +76,7 @@ class TodoData with ChangeNotifier {
     notifyListeners();
     await TodoApi().updateTodoRequest(todo.id, requestData);
   }
+
 
   void updateTodoName(Todo todo, String updateName) async {
     Map<String, dynamic> requestData = {
@@ -83,8 +90,9 @@ class TodoData with ChangeNotifier {
     await TodoApi().updateTodoRequest(todo.id, requestData);
   }
 
+
   void deleteTodo(Todo todo) async {
-    _todos.remove(todo);
+    _todoMap[todo.dueDate]!.remove(todo);
     notifyListeners();
     await TodoApi().deleteTodoRequest(todo.id);
   }
