6a65e69bb8b53ac4105851566c3250aea43e946c
jiwon79 <19-079@ksa.hs.kr>
2022-02-16 23:55:56 +0900

    refactor: refactor follow item by using spacer

--- lib/widgets/follow/follow_item.dart
+++ lib/widgets/follow/follow_item.dart
@@ -25,42 +25,39 @@ class FollowItem extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              Container(
-                width: 60,
-                margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(user.username,
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontFamily: 'noto',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(user.nickname,
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'noto',
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xff989898),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          Container(
+            width: 60,
+            margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(30),
+            ),
           ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(user.username,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'noto',
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(user.nickname,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'noto',
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff989898),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
           followButton(context, option, user),
         ],
       ),
